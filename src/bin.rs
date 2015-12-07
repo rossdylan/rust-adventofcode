683102b3ffa644832fc2d2c4e2913589b6a825b1
Ross Delinger <rossdylan@csh.rit.edu>
2015-12-07 16:42:35 -0500

    Added a command line wrapper to run the actual challenges

--- src/bin.rs
+++ src/bin.rs
@@ -1,14 +1,16 @@
 #![feature(test)]
+
 #![feature(iter_arith)]
 #![feature(convert)]
 #[macro_use]
 
 extern crate nom;
+extern crate clap;
 
 mod day2;
 
 use std::io::{stdin, Read};
-
+use clap::{App, SubCommand};
 
 
 fn run_day2() {
@@ -20,6 +22,23 @@ fn run_day2() {
     println!("[Day 2] Part 2: {}", rib);
 }
 
+fn run_day3() {
+    let mut buffer : Vec<u8> = vec![];
+    stdin().read_to_end(&mut buffer).unwrap();
+}
+
 pub fn main() {
-    run_day2();
+    let matches = App::new("adventofcode")
+        .version("0.1.0")
+        .author("Ross Delinger <rossdylan@fastmail.com>")
+        .about("Program for running my adventofcode submissions")
+        .subcommand(SubCommand::with_name("day2"))
+        .subcommand(SubCommand::with_name("day3")).get_matches();
+
+    if let Some(_) = matches.subcommand_matches("day2") {
+        run_day2();
+    }
+    if let Some(_) = matches.subcommand_matches("day3") {
+        run_day3();
+    }
 }
